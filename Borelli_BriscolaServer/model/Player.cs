5047bf1ca746ad93403084c749fd762050cb5eed
marcoborelli <marco05.borelli@gmail.com>
2024-02-12 22:49:32 +0100

    model\Player: modificato costruttore: adesso accetta una lista di carte; aggiunto attributo Socket all'interno della classe

--- Borelli_BriscolaServer/model/Player.cs
+++ Borelli_BriscolaServer/model/Player.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 
 namespace Borelli_BriscolaServer.model {
     public class Player {
         public string Name { get; private set; }
         public List<Card> Hand { get; private set; }
         public byte Score { get; internal set; }
+        public TcpClient ClientSocket { get; set; }
 
-        public Player(string name, Card[] initHand) {
+        public Player(string name, TcpClient socket, List<Card> initHand) {
             Name = name;
             Score = 0;
 
-            if (initHand.Length != 3) {
+            if (initHand.Count != 3) {
                 throw new Exception("Carte inziali non valide");
             }
-            Hand = new List<Card>() { initHand[0], initHand[1], initHand[2] };
+            Hand = initHand;
+
+            ClientSocket = socket;
         }
 
         public Card PlayCard(byte index) {
